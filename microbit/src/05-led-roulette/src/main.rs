011e8b10ac05e2c8b3d4906d6eadce58b8fea4ca
Nick Mulder <nmulder@outlook.com>
2023-02-11 14:57:03 -0600

    starting solution 05

--- microbit/src/05-led-roulette/src/main.rs
+++ microbit/src/05-led-roulette/src/main.rs
@@ -3,15 +3,43 @@
 #![no_std]
 
 use cortex_m_rt::entry;
-use panic_halt as _;
-use microbit as _;
+use rtt_target::rtt_init_print;
+use panic_rtt_target as _;
+use microbit::{
+    board::Board,
+    display::blocking::Display,
+    hal::{prelude::*, Timer},
+};
 
 #[entry]
 fn main() -> ! {
-    let _y;
-    let x = 42;
-    _y = x;
+    rtt_init_print!();
 
-    // infinite loop; just so we don't leave this stack frame
-    loop {}
+    let board = Board::take().unwrap();
+    let mut timer = Timer::new(board.TIMER0);
+    let mut display = Display::new(board.display_pins);
+    let mut grid = [
+        [1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+    ];
+
+    loop {
+        // Show light_it_all for 1000ms
+        display.show(&mut timer, grid, 1000);
+        let mut pos: (u8, u8) = (0,0);
+
+        // Get lit node
+        for (i, row) in grid {
+            for (j, col) in row {
+                if col == 1 {
+                    pos = (i, j);
+                }
+            }
+        }
+
+        timer.delay_ms(1000_u32);
+    }
 }
